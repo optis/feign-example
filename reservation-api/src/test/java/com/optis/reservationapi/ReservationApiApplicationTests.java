b374286a22b567712fe0f21d3549b1619b5e7065
Aiko Beyers <aiko.beyers@optis.be>
2019-02-15 14:33:51 +0100

    general folder rename, post method fix

--- reservation-api/src/test/java/com/optis/reservationapi/ReservationApiApplicationTests.java
+++ reservation-api/src/test/java/com/optis/reservationapi/ReservationApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FeignDemoApiApplicationTests {
+public class ReservationApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
